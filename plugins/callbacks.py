6ac8818969a1fd3e4dd73663aee3aef22cfc6348
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-01-19 06:14:35 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy? go and ask him", url="https://t.me/akalankanime"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Moveis Channel", url="https://t.me/reviewhmofficial"),
+                    InlineKeyboardButton("Moveis Group", url="https://t.me/HMOFFICIALMovies")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
