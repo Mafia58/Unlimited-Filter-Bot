c852e4d56e78441f6ba4de44c08ec4720a8ca0b0
Nimesh Akalanka <90953455+Mafia58@users.noreply.github.com>
2022-01-19 06:17:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy? go and ask him", url="https://t.me/akalankanime"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Moveis Channel", url="https://t.me/reviewhmofficial"),
+                    InlineKeyboardButton("Moveis Group", url="https://t.me/HMOFFICIALMovies")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
